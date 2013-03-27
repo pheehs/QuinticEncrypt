bec90ce2975a0ca21e90280ee8d6c542ed9641a6
pheehs <ring0pycode@gmail.com>
2013-03-28 00:05:51 +0900

    add decrypt() and arrange document

--- README.md
+++ README.md
@@ -1,52 +1,160 @@
 ## Basic Concept
+Inspired by Tatsuyuki Inoue
+
     ( x - (±α_1 ±α_2 *i) )* ( x - (±α_3 ±α_4 *i) )*
     ( x - (±α_5 ±α_6 *i) )* ( x - (±α_7 ±α_8 *i) )*
 	
-	( x - (±β_1 ±β_2 *i) )* ( x - (±β_3 ±β_4 *i) )
+	( x - (±β_1 ±β_2 *i) )* ( x - (±β_3 ±β_4 *i) )* ...
 
-α_1 〜 α_8, β_1 〜 β_4 (NUM_OF_KEYS * 2個)
+    α_1 〜 α_8： ±AABBCCDDEEZ // 平文
+    | ±: AAの最上位ビットが1なら-、0なら+ // 要らない？
+    | AA - EE: 平文の分割されたデータ(長さ：param_chars)
+    | Z: 順番に 0 〜 7 // 復号化時に平文を再構築するため
+		
+    β_1 〜 β_(NUM_OF_KEYS*2): ±AABBCCDDEEZ // 鍵
+    | ±: AAの最上位ビットが1なら-、0なら+
+    | AA...: 鍵のSHA512の上位(PARAM_CHARS)*(NUM_OF_KEYS*2)バイト  
+    |        長さが足りなくなったらhashの更にhashをとってつなげる。
+	|		 平文の方と同じ長さ
+    | Z: 順番に 0 ~ 7
+	
+といった感じに平文と鍵を表す複素数の根をもつ式を展開し、その係数のみを取ることで暗号化するブロック暗号。  
+効率的な因数分解アルゴリズムは色々あるが、5次以上の方程式の解の公式は存在しないことを利用する。  
+平文のみの式は4次式になるようにし、鍵を組み合わせることで5次式以上にする。  
+復号化するときは鍵が根になることを利用して4次式に次数下げして、解く。  
 
-α_k： ±AABBCCDDEEZ // 平文
-| ±: AAの最上位ビットが1なら-、0なら+ // 要らない？
-| AA - EE: データ
-| Z: 組立番号  0000(0x0) - 1000(0x8)
-β_k: ±AABBCCDDEEZ // 鍵
-| ±: AAの最上位ビットが1なら-、0なら+
-| AA...: 鍵のSHA512の上位5*(NUM_OF_KEYS*2)バイト  
-         [64/10=6.4]よりNUM_OF_KEYSは最大で6まで　
-		 と思いきや足りなくなったらhashの更にhashをとってつなげて、続ける。
-| Z: ランダムに 0 ~ 8
+数学的な部分のアルゴリズムとか面倒くさそうだったので[Maxima](http://maxima.sourceforge.net/)を呼び出して解いてもらう。  
+自分のMac環境での`subprocess`の呼び出し方なので、他の環境なら`MAXIMA_EXEC`を変えるべし。  
 
 ## Structure of Encrypted file
-|                                equation_1                              ||    equation_2 ..
-|         header        ||                body                           ||
-|ndlen| ... |2dlen|1dlen||nd-img,nd-rel| ... |2d-img,2d-rel|1d-img,1d-rel|| 
-      ^     ^     ^     ^       ^                   ^             ^
-	 NULL  NULL  NULL  NULL    SPACE              SPACE        SPACE
+
+    |length|param|                                              equation_1                                                    ||    equation_2 ..
+    |of org|chars|                               header                      ||                body                           ||
+    |data  |     |dim|nd-imglen|nd-rellen| ... |2d-rellen|1d-imglen|1d-rellen||nd-img|nd-rel| ... |2d-img|2d-rel|1d-img|1d-rel||
+    |4bytes|  2  | 2 |  4      |    4    |     |     4   |    4    |     4   ||  ?   |   ?  |     |  ?   |  ?   |   ?  |   ?  ||
+
+## BUGs and Matters of concern
+1. たまにMaximaがこんなエラー吐いて落ちる。
+
+    factor: ran out of primes.
+    -- an error. To debug this try: debugmode(true);)`
+
+  [ここ](http://comments.gmane.org/gmane.comp.mathematics.maxima.general/23175)でなんか言ってるけどよくわからん。  
+
+2. **ゴリ押しで因数分解される可能性が十分ある。**
+  一応鍵と平文の区別がつかないようになっているが、(下位8bitが平文と鍵で同じものがある）
+  因数分解できる状況で同じ平文と同じ鍵に対する暗号文を知られると鍵が分かるので、完全に解読される。
+  -> どれぐらい次数を上げればで計算量的安全性を確保できるか？
+3. 暗号化後のデータサイズが大きい
+
+## Benchmark
+|##                              |   user|       sys|     total|      real|
+|:-------------------------------|------:|---------:|---------:|---------:|
+|encrypt/decrypt(p: 3, k: 1)     | 0.0500|    0.0600|    0.1100|   11.5441|
+|encrypt/decrypt(p: 3, k: 2)     | 0.0500|    0.0600|    0.1100|   11.2701|
+|encrypt/decrypt(p: 3, k: 3)     | 0.0500|    0.0500|    0.1000|   11.8981|
+|encrypt/decrypt(p: 3, k: 4)     | 0.0700|    0.0600|    0.1300|   12.8204|
+|encrypt/decrypt(p: 3, k: 5)     | 0.0800|    0.0700|    0.1500|   13.2430|
+|encrypt/decrypt(p: 3, k: 6)     | 0.0800|    0.0600|    0.1400|   12.9392|
+|encrypt/decrypt(p: 3, k: 7)     | 0.1000|    0.0600|    0.1600|   12.9341|
+|encrypt/decrypt(p: 3, k: 8)     | 0.1000|    0.0600|    0.1600|   13.2537|
+|encrypt/decrypt(p: 3, k: 9)     | 0.1300|    0.0600|    0.1900|   14.4122|
+|encrypt/decrypt(p: 3, k: 10)    | 0.1500|    0.0600|    0.2100|   14.3255|
+|encrypt/decrypt(p: 4, k: 1)     | 0.0300|    0.0500|    0.0800|    8.3306|
+|encrypt/decrypt(p: 4, k: 2)     | 0.0400|    0.0400|    0.0800|    8.7008|
+|encrypt/decrypt(p: 4, k: 3)     | 0.0400|    0.0500|    0.0900|    8.7001|
+|encrypt/decrypt(p: 4, k: 4)     | 0.0600|    0.0500|    0.1100|    8.9509|
+|encrypt/decrypt(p: 4, k: 5)     | 0.0600|    0.0400|    0.1000|    9.0792|
+|encrypt/decrypt(p: 4, k: 6)     | 0.0800|    0.0500|    0.1300|    9.9843|
+|encrypt/decrypt(p: 4, k: 7)     | 0.0900|    0.0500|    0.1400|   10.6747|
+|encrypt/decrypt(p: 4, k: 8)     | 0.1000|    0.0500|    0.1500|   10.4102|
+|encrypt/decrypt(p: 4, k: 9)     | 0.1100|    0.0400|    0.1500|   10.2570|
+|encrypt/decrypt(p: 4, k: 10)    | 0.1200|    0.0500|    0.1700|   10.8880|
+|encrypt/decrypt(p: 5, k: 1)     | 0.0400|    0.0400|    0.0800|    7.3138|
+|encrypt/decrypt(p: 5, k: 2)     | 0.0400|    0.0400|    0.0800|    7.3473|
+|encrypt/decrypt(p: 5, k: 3)     | 0.0500|    0.0400|    0.0900|    7.4862|
+|encrypt/decrypt(p: 5, k: 4)     | 0.0500|    0.0400|    0.0900|    7.7534|
+|encrypt/decrypt(p: 5, k: 5)     | 0.0600|    0.0400|    0.1000|    7.9698|
+|encrypt/decrypt(p: 5, k: 6)     | 0.0700|    0.0400|    0.1100|    8.3844|
+|encrypt/decrypt(p: 5, k: 7)     | 0.0800|    0.0300|    0.1100|    8.1864|
+|encrypt/decrypt(p: 5, k: 8)     | 0.1000|    0.0400|    0.1400|    8.7232|
+|encrypt/decrypt(p: 5, k: 9)     | 0.1100|    0.0400|    0.1500|    9.8737|
+|encrypt/decrypt(p: 5, k: 10)    | 0.1500|    0.0500|    0.2000|   10.4016|
+|encrypt/decrypt(p: 6, k: 1)     | 0.0300|    0.0300|    0.0600|    6.2429|
+|encrypt/decrypt(p: 6, k: 2)     | 0.0400|    0.0300|    0.0700|    6.3985|
+|encrypt/decrypt(p: 6, k: 3)     | 0.0400|    0.0300|    0.0700|    6.3422|
+|encrypt/decrypt(p: 6, k: 4)     | 0.0500|    0.0400|    0.0900|    6.4226|
+|encrypt/decrypt(p: 6, k: 5)     | 0.0600|    0.0300|    0.0900|    6.4196|
+|encrypt/decrypt(p: 6, k: 6)     | 0.0800|    0.0300|    0.1100|    6.6668|
+|encrypt/decrypt(p: 6, k: 7)     | 0.0900|    0.0300|    0.1200|    9.0652|
+|encrypt/decrypt(p: 6, k: 8)     | 0.1000|    0.0400|    0.1400|    9.2325|
+|encrypt/decrypt(p: 6, k: 9)     | 0.1200|    0.0300|    0.1500|    8.1738|
+|encrypt/decrypt(p: 6, k: 10)    | 0.1300|    0.0300|    0.1600|    8.9072|
+|encrypt/decrypt(p: 7, k: 1)     | 0.0300|    0.0300|    0.0600|    5.9632|
+|encrypt/decrypt(p: 7, k: 2)     | 0.0400|    0.0300|    0.0700|    5.3397|
+|encrypt/decrypt(p: 7, k: 3)     | 0.0400|    0.0200|    0.0600|    5.3549|
+|encrypt/decrypt(p: 7, k: 4)     | 0.0500|    0.0300|    0.0800|    5.3711|
+|encrypt/decrypt(p: 7, k: 5)     | 0.0600|    0.0200|    0.0800|    5.3618|
+|encrypt/decrypt(p: 7, k: 6)     | 0.0700|    0.0300|    0.1000|    5.6050|
+|encrypt/decrypt(p: 7, k: 7)     | 0.0700|    0.0300|    0.1000|    5.6503|
+|encrypt/decrypt(p: 7, k: 8)     | 0.0900|    0.0200|    0.1100|    6.1006|
+|encrypt/decrypt(p: 7, k: 9)     | 0.1000|    0.0300|    0.1300|    5.9557|
+|encrypt/decrypt(p: 7, k: 10)    | 0.1200|    0.0300|    0.1500|    6.3328|
+|encrypt/decrypt(p: 8, k: 1)     | 0.0300|    0.0200|    0.0500|    4.4236|
+|encrypt/decrypt(p: 8, k: 2)     | 0.0400|    0.0300|    0.0700|    4.6038|
+|encrypt/decrypt(p: 8, k: 3)     | 0.0300|    0.0200|    0.0500|    4.7485|
+|encrypt/decrypt(p: 8, k: 4)     | 0.0400|    0.0200|    0.0600|    4.8080|
+|encrypt/decrypt(p: 8, k: 5)     | 0.0700|    0.0300|    0.1000|    4.8728|
+|encrypt/decrypt(p: 8, k: 6)     | 0.0700|    0.0200|    0.0900|    5.1923|
+|encrypt/decrypt(p: 8, k: 7)     | 0.0900|    0.0300|    0.1200|    5.1286|
+|encrypt/decrypt(p: 8, k: 8)     | 0.1000|    0.0200|    0.1200|    5.4628|
+|encrypt/decrypt(p: 8, k: 9)     | 0.1100|    0.0300|    0.1400|    5.4705|
+|encrypt/decrypt(p: 8, k: 10)    | 0.1400|    0.0200|    0.1600|    5.6909|
+|encrypt/decrypt(p: 9, k: 1)     | 0.0200|    0.0300|    0.0500|    4.1901|
+|encrypt/decrypt(p: 9, k: 2)     | 0.0300|    0.0200|    0.0500|    4.5074|
+|encrypt/decrypt(p: 9, k: 3)     | 0.0400|    0.0200|    0.0600|    4.5240|
+|encrypt/decrypt(p: 9, k: 4)     | 0.0500|    0.0200|    0.0700|    4.4568|
+|encrypt/decrypt(p: 9, k: 5)     | 0.0600|    0.0300|    0.0900|    4.6615|
+|encrypt/decrypt(p: 9, k: 6)     | 0.0800|    0.0200|    0.1000|    4.5378|
+|encrypt/decrypt(p: 9, k: 7)     | 0.0800|    0.0200|    0.1000|    4.8666|
+|encrypt/decrypt(p: 9, k: 8)     | 0.1000|    0.0300|    0.1300|    4.9316|
+|encrypt/decrypt(p: 9, k: 9)     | 0.1200|    0.0200|    0.1400|    5.0116|
+|encrypt/decrypt(p: 9, k: 10)    | 0.1400|    0.0200|    0.1600|    5.1788|
+|encrypt/decrypt(p: 10, k: 1)    | 0.0300|    0.0200|    0.0500|    3.7347|
+|encrypt/decrypt(p: 10, k: 2)    | 0.0300|    0.0200|    0.0500|    3.8614|
+|encrypt/decrypt(p: 10, k: 3)    | 0.0400|    0.0200|    0.0600|    4.0064|
+|encrypt/decrypt(p: 10, k: 4)    | 0.0500|    0.0200|    0.0700|    4.0462|
+|encrypt/decrypt(p: 10, k: 5)    | 0.0600|    0.0200|    0.0800|    4.1064|
+|encrypt/decrypt(p: 10, k: 6)    | 0.0700|    0.0200|    0.0900|    4.2203|
+|encrypt/decrypt(p: 10, k: 7)    | 0.0800|    0.0200|    0.1000|    4.2450|
+|encrypt/decrypt(p: 10, k: 8)    | 0.1100|    0.0200|    0.1300|    4.5015|
+|encrypt/decrypt(p: 10, k: 9)    | 0.1300|    0.0300|    0.1600|    4.6233|
+|encrypt/decrypt(p: 10, k: 10)   | 0.1500|    0.0200|    0.1700|    4.7639|
 
 ## Notes
-a polynomial expression 多項式
-high-powered equation 高次方程式
+* もともと5次方程式を利用するつもりだったが、拡張して次数が自由に変えられるので名前があってない。
 
-5次方程式
-quintic equation
+a polynomial expression 多項式  
+high-powered equation 高次方程式  
 
-↓字数q(ﾟдﾟ )↓sage↓
+5次方程式  
+quintic equation  
 
-4次方程式
-quartic equation
+↓*次数q(ﾟдﾟ )↓sage↓*
+
+4次方程式  
+quartic equation  
 
 1. 複二次式の場合
   二次式と同様に解ける
 2. それ以外の場合
-  x^3の項を消して、フェラーリの方法orデカルトの方法orオイラーの方法で3次式に
-(http://www004.upp.so-net.ne.jp/s_honma/ferrari/ferrari.htm)
-(http://ja.wikipedia.org/wiki/4%E6%AC%A1%E6%96%B9%E7%A8%8B%E5%BC%8F)
+  x^3の項を消して、フェラーリの方法orデカルトの方法orオイラーの方法で3次式と2次式に
+  参考:[wikipedia](http://ja.wikipedia.org/wiki/4%E6%AC%A1%E6%96%B9%E7%A8%8B%E5%BC%8F)など
 
-↓字数q(ﾟдﾟ )↓sage↓
+↓*次数q(ﾟдﾟ )↓sage↓*
 
-3次方程式
-cubic equation
+3次方程式  
+cubic equation  
 
-2次方程式
-quadratic equation
+2次方程式  
+quadratic equation  
--- quintic_encrypt.py
+++ quintic_encrypt.py
@@ -3,14 +3,11 @@
 
 from subprocess import Popen, PIPE
 from hashlib import sha512
-from random import sample, randint
+from random import sample
+from struct import pack, unpack
 import re
 
-# each solution parameter contains plain data(PARAM_CHARS byte) and order number(8bit)
-# so, if PARAM_CHARS=5, it can be 0xFFFFFFFFFFF = 17592186044415 at most
-PARAM_CHARS = 5
 JUNK = "\xff" # for padding
-NUM_OF_KEYS = 2 # encrypted equation will be (4+NUM_OF_KEYS)-dimensional
 
 MAXIMA_EXEC = ["/Applications/Maxima.app/Contents/Resources/maxima/bin/sbcl", # on my mac os
                "--core",
@@ -18,58 +15,62 @@ MAXIMA_EXEC = ["/Applications/Maxima.app/Contents/Resources/maxima/bin/sbcl", # 
                "--noinform", "--end-runtime-options", "--eval", "(cl-user::run)", "--end-toplevel-options",
                "-q", "--disable-readline",] # "--batch-string="
 
-# Maxima batch string
-EXPAND_POLY = "display2d:false$linel:10000$" +\
-              "poly:expand((x-(%+d%+d*%%i))*(x-(%+d%+d*%%i))*(x-(%+d%+d*%%i))*(x-(%+d%+d*%%i))" + "*(x-(%+d%+d*%%i))"*NUM_OF_KEYS + ")$\n" +\
-              "[" + ",".join(["ratsimp(coeff(poly, x, %d))"%i for i in xrange(4+NUM_OF_KEYS-1, -1, -1)]) + "];" # to encrypt
-SOLVE_QUARTIC = "display2d:false$linel:10000$" +\
-                "quartic:divide(x^%d" % (4+NUM_OF_KEYS) + "".join(["%+d*x^"+"%d"%i for i in xrange(4+NUM_OF_KEYS-1, -1, -1)]) + ", " "*".join(["(x%+d)" for i in xrange(NUM_OF_KEYS)]) + ")$\n" +\
-                "if quartic[2]#0 then print(\"ERROR!\"), quit();\n" +\
-                "solve(gfactor(quartic[1]));"
-# to decrypt with key
-### key must flip sign
-
 # regular expressions for Maxima output 
 COMPLEX_RE = re.compile(r"(?P<sign1>[-]?)(?P<imgnum>\d+)[*]%i(?P<sign2>[+\-])(?P<realnum>\d+)")
-COMPLEX_RE2 = re.compile(r"(?P<sign1>[-]?)(?P<realnum>\d+)(?P<sign2>[+\-])(?P<imgnum>\d+)[*]%i")
+COMPLEX_RE2 = re.compile(r"(?P<sign2>[-]?)(?P<realnum>\d+)(?P<sign1>[+\-])(?P<imgnum>\d+)[*]%i")
 
 
 class HighPowered(object):
-    def __init__(self, plaindata, debug=False):
-        # plaindata : str
+    def __init__(self, debug=False, param_chars=5, num_of_keys=2):
         self.debug = debug
-        self.plaindata = plaindata + JUNK * (PARAM_CHARS*8 - len(plaindata) % (PARAM_CHARS*8))
+        self.quiet = False
+        # each solution parameter contains plain data(self.param_chars byte) and order number(8bit)
+        # so, if self.param_chars=5, it can be 0xFFFFFFFFFFF = 17592186044415 at most
+        self.param_chars = param_chars
+        self.num_of_keys = num_of_keys # encrypted equation will be (4+self.num_of_keys)-dimensional
 
-    def encrypt(self, key, encfile="encdata.bin"):
+    # Maxima batch string
+    def EXPAND_POLY(self):
+        return "display2d:false$linel:1000000$" +\
+            "poly:expand((x-(%+d%+d*%%i))*(x-(%+d%+d*%%i))*(x-(%+d%+d*%%i))*(x-(%+d%+d*%%i))" + "*(x-(%+d%+d*%%i))"*self.num_of_keys + ")$\n" +\
+            "[" + ",".join(["ratsimp(coeff(poly, x, %d))"%i for i in xrange(4+self.num_of_keys-1, -1, -1)]) + "];" # to encrypt
+    def SOLVE_QUARTIC(self):
+        return "display2d:false$linel:1000000$" +\
+            "quartic:divide(x^%d" % (4+self.num_of_keys) + "".join(["+(%+d%+d*%%i)*x^"+"%d"%i for i in xrange(4+self.num_of_keys-1, -1, -1)]) + ", " + "*".join(["(x-(%+d%+d*%%i))" for i in xrange(self.num_of_keys)]) + ")$\n" +\
+            "if quartic[2]#0 then print(\"ERROR!\") else solve(gfactor(quartic[1]));" # to decrypt with key
+
+    def encrypt(self, plaindata, key):
         # key : str
-        # encfile : str of file path to save the encrypted data
-
+        # plaindata : data to encrypt
+        org_length = len(plaindata)
+        plaindata = plaindata + JUNK * (self.param_chars*8 - len(plaindata) % (self.param_chars*8))
+        
         # create equations regarding data/key as solution parameters
         sol_params = []
         # data to params
-        for i,c in enumerate(self.plaindata):
-            if i % (8*PARAM_CHARS) == 0: # new equation
+        for i,c in enumerate(plaindata):
+            if i % (8*self.param_chars) == 0: # new equation
                 sol_params.append([j for j in xrange(8)])
-            sol_params[-1][(i%(PARAM_CHARS*8))/PARAM_CHARS] += ord(c)*0x100 ** (PARAM_CHARS - (i % PARAM_CHARS) - 1) * 0x10
-        if self.debug:
-            print "[*] sol_params(hex): [" + ", ".join(["["+", ".join([hex(c) for c in param])+"]" for param in sol_params]) + "]"
-            #print "[*] sol_params(deci):", sol_params
+            sol_params[-1][(i%(self.param_chars*8))/self.param_chars] += ord(c)*0x100 ** (self.param_chars - (i % self.param_chars) - 1) * 0x10
+        #if self.debug:
+        #    print "[*] sol_params(hex): [" + ", ".join(["["+", ".join([hex(c) for c in param])+"]" for param in sol_params]) + "]"
+        #    #print "[*] sol_params(deci):", sol_params
 
         # shuffle params list => 8!(=479001600) patterns
         sol_params = [sample(param, 8) for param in sol_params]
         # key to params
         keyhash = sha512(key).hexdigest()
         if self.debug: print "[*] keyhash:", keyhash
-        while len(keyhash) < NUM_OF_KEYS * 10:
+        while len(keyhash) < self.param_chars * self.num_of_keys * 2 * 2:
             keyhash += sha512(keyhash).hexdigest()
             if self.debug: print "[*] keyhash:", keyhash
         for i in xrange(len(sol_params)):
-            for k in xrange(NUM_OF_KEYS*2):
-                sol_params[i].append(int(keyhash[10*k:10+10*k], 16)*0x10 + randint(0, 8))
+            for k in xrange(self.num_of_keys*2): # in each equation
+                sol_params[i].append(int(keyhash[self.param_chars*2*k:self.param_chars*2*(k+1)], 16)*0x10 + (k % 8))
         
         # check plus or minus
         for i in xrange(len(sol_params)):
-            for j in xrange(8+NUM_OF_KEYS*2):
+            for j in xrange(8+self.num_of_keys*2):
                 if sol_params[i][j] >> 39:
                     sol_params[i][j] = -sol_params[i][j]
         if self.debug:
@@ -80,8 +81,7 @@ class HighPowered(object):
         coefficients = []
         for param in sol_params:
             coefficients.append([])
-            output = self.run_maxima(EXPAND_POLY % tuple(param))
-            #print output
+            output = self.run_maxima(self.EXPAND_POLY() % tuple(param))
             coeff_str_list = output.split("(%o4)")[-1].strip()[1:-1].split(",")
             for coeff_str in coeff_str_list:
                 r = COMPLEX_RE.match(coeff_str.strip())
@@ -89,30 +89,192 @@ class HighPowered(object):
                     r = COMPLEX_RE2.match(coeff_str.strip())
                     if not r:
                         print "[!]", coeff_str.strip(), r
-                        raise TypeError
-                coefficients[-1].append((int(r.group("sign1")+r.group("imgnum")), int(r.group("sign2")+r.group("realnum"))))
+                        raise TypeError, "regular expression unmatch"
+                coefficients[-1].append(int(r.group("sign2")+r.group("realnum")))
+                coefficients[-1].append(int(r.group("sign1")+r.group("imgnum")))
 
-        print "[*] creating encrypted file..."
-        fd = open(encfile, "wb")
+        encdata = pack("<IH", org_length, self.param_chars) # length of original data
         for coeffs in coefficients:
-            equ_header = self.int2bin(len(coeffs))+"\x00"
+            equ_header = pack("<H", len(coeffs)/2) # dimension of the equation
             equ_body = ""
             cur = 0
             for coeff in coeffs:
-                equ_body += self.int2bin(coeff[0])
-                equ_header += self.int2bin(len(equ_body) - cur) + "\x00"
+                equ_body += self.int2bin(coeff) # coefficient
+                equ_header += pack("<I", len(equ_body) - cur) # length of int2bin(coefficient)
                 cur = len(equ_body)
-                equ_body += self.int2bin(coeff[1])
-                equ_header += self.int2bin(len(equ_body) - cur) + "\x00"
-                cur = len(equ_body)
-            fd.write(equ_header + equ_body)
-        print "[*] plain data size:", len(self.plaindata)
-        print "[*] encrypted size:", fd.tell()
-        fd.close()
-        print "[*] file saved!"
-        print 
-        print 
-        return
+            encdata += (equ_header + equ_body)
+        return encdata
+
+    def encrypt_with_file(self, plaindata=None, plainfile=None, keydata=None, keyfile=None, encfile=None):
+        # plainfile : str of file path to encrypt -> plaindata
+        # keyfile : str of file path to use as key to encrypt -> keydata
+        # encfile : str of file path to save the encrypted data
+
+        if not self.quiet:
+            print 
+            print "< ENCRYPT MODE >"
+
+        if not plaindata and plainfile:
+            if not self.quiet: print "[*] plainfile: %s" % plainfile
+            fd = open(plainfile, "rb")
+            plaindata = fd.read()
+            fd.close()
+        elif plaindata:
+            if not self.quiet: print "[*] plain data: %s" % plaindata
+        else:
+            raise AssertionError, "plaindata or plainfile required"
+
+        if not keydata and keyfile:
+            if not self.quiet: print "[*] keyfile: %s" % keyfile
+            fd = open(keyfile, "rb")
+            keydata = fd.read()
+            fd.close()
+        elif keydata:
+            if not self.quiet: print "[*] keydata: %s" % keydata
+        else:
+            raise AssertionError, "keydata or keyfile required"
+
+        encdata = self.encrypt(plaindata, keydata)
+        if encfile:
+            if not self.quiet: print "[*] creating encrypted file('%s')..." % encfile
+            fd = open(encfile, "wb")
+            fd.write(encdata)
+            fd.close()
+            if not self.quiet: print "[*] file saved!"
+
+        if not self.quiet:
+            print "[*] plain data size:", len(plaindata)
+            print "[*] encrypted size:", len(encdata)
+            print
+        if not encfile:
+            return encdata
+        else:
+            return
+
+    def decrypt(self, encdata, key):
+        # key: str
+        # encdata : data to decrypt
+        
+        # load length/coefficients from encdata
+        c = 6 # encdata counter
+        org_length,self.param_chars = unpack("<IH", encdata[:6])
+        coefficients = []
+        while c < len(encdata):
+            coefficients.append([])
+            dim = unpack("<H", encdata[c:c+2])[0] # dimension of the equation
+            c += 2
+            len_coeffs = []
+            for i in xrange(dim*2):
+                len_coeffs.append(unpack("<I", encdata[c:c+4])[0]) # length of the coefficient
+                c += 4
+            for l in len_coeffs:
+                coefficients[-1].append(self.bin2int(encdata[c:c+l]))
+                c += l
+        self.num_of_params = dim - 4
+        # get key from sha512 hash of key
+        keyhash = sha512(key).hexdigest()
+        if self.debug: print "[*] keyhash:", keyhash
+        while len(keyhash) < self.param_chars * self.num_of_keys * 2 * 2:
+            keyhash += sha512(keyhash).hexdigest()
+            if self.debug: print "[*] keyhash:", keyhash
+        sol_keys = []
+        for i in xrange(len(coefficients)):
+            sol_keys.append([])
+            for k in xrange(self.num_of_keys*2): # in each equation
+                sol_keys[-1].append(int(keyhash[self.param_chars*2*k:self.param_chars*2*(k+1)], 16)*0x10 + (k % 8))
+        # check plus or minus
+        for i in xrange(len(sol_keys)):
+            for j in xrange(self.num_of_keys*2):
+                if sol_keys[i][j] >> 39:
+                    sol_keys[i][j] = -sol_keys[i][j]
+
+        # lowering degree with key and solve quartic
+        sol_params = []
+        for coeffs,keys in zip(coefficients, sol_keys):
+            sol_params.append([])
+            output = self.run_maxima(self.SOLVE_QUARTIC() % tuple(coeffs + keys))
+            output = output.split("(%o4)")[-1].strip()
+            if "ERROR!" in output:
+                raise KeyError, "incorrect key or broken encrypted data(%s)" % output
+            sol_str_list = output[1:-1].split(",")
+            for sol_str in sol_str_list:
+                r = COMPLEX_RE.match(sol_str[4:].strip())
+                if not r:
+                    r = COMPLEX_RE2.match(sol_str[4:].strip())
+                    if not r:
+                        print "[!]", sol_str[4:].strip(), r
+                        raise TypeError, "regular expression unmatch"
+                sol_params[-1].append(int(r.group("realnum"))) # ignore sign
+                sol_params[-1].append(int(r.group("imgnum")))
+        # sort solution parameters
+        for i,params in enumerate(sol_params):
+            for j,p in enumerate(params):
+                sol_params[i][j] = (p % 0x10, p / 0x10)
+            sol_params[i].sort()
+        # decode int to string
+        decdata = ""
+        for params in sol_params:
+            for p in params:
+                param_chr = ""
+                num = p[1]
+                while num > 255:
+                    param_chr = chr(num % 256) + param_chr
+                    num /= 256
+                decdata += (chr(num) + param_chr)
+
+        # check correctness and cut JUNK data
+        if decdata[org_length:] == JUNK*(len(decdata)-org_length):
+            decdata = decdata[:org_length]
+        else:
+            raise TypeError, "decryption succeeded but data is broken"
+        return decdata
+        
+    def decrypt_with_file(self, encdata=None, encfile=None, keydata=None, keyfile=None, decfile=None):
+        # encfile : str of file path to decrypt -> encdata
+        # keyfile: str of file path to use as key to decrypt -> keydata
+        # decfile: str of file path to save the decrypted data
+        
+        if not self.quiet:
+            print
+            print "< DECRYPT MODE >"
+
+        if not encdata and encfile:
+            if not self.quiet: print "[*] encfile: %s" % encfile
+            fd = open(encfile, "rb")
+            encdata = fd.read()
+            fd.close()
+        elif encdata:
+            if not self.quiet: print "[*] encdata: %s" % encdata
+        else:
+            raise AssertionError, "encdata or encfile required"
+
+        if not keydata and keyfile:
+            if not self.quiet: print "[*] keyfile: %s" % keyfile
+            fd = open(keyfile, "rb")
+            keydata = fd.read()
+            fd.close()
+        elif keydata:
+            if not self.quiet: print "[*] keydata: %s" % keydata
+        else:
+            raise AssertionError, "keydata or keyfile required"
+
+        decdata = self.decrypt(encdata, keydata)
+        if decfile:
+            if not self.quiet: print "[*] creating decrypted file('%s')..." % decfile
+            fd = open(decfile, "wb")
+            fd.write(decdata)
+            fd.close()
+            if not self.quiet: print "[*] file saved!"
+            
+        if not self.quiet:
+            print "[*] encrypted data size:", len(encdata)
+            print "[*] decrypted size:", len(decdata)
+            print
+        if not decfile:
+            return decdata
+        else:
+            return
+
 
     def int2bin(self, num):
         if self.debug:
@@ -122,19 +284,29 @@ class HighPowered(object):
             data = "-"
             num = -num
         else:
-            data = ""
+            data = "+"
         while num > 255:
             data = chr(num % 256) + data
             num /= 256
         data = chr(num) + data
         return data
+
+    def bin2int(self, bin):
+        num = 0
+        for i,c in enumerate(bin[-2::-1]):
+            num += ord(c) * (256**i)
+        if bin[-1] == "-":
+            num = -num
+        if self.debug:
+            #print "[*] bin2int(bin) = ", hex(num)
+            pass
+        return num
         
     def run_maxima(self, batch):
         # batch : batch string for maxima
         if self.debug:
             print "[*] "+"="*19 +"maxima:"+"="*20
             print batch
-            print "="*50
         p = Popen(MAXIMA_EXEC+["--batch-string="+batch], stdout=PIPE)
         output = p.stdout.read()
         p.wait()
@@ -144,7 +316,34 @@ class HighPowered(object):
             print "="*50, "\n"
         return output
 
+    def benchmark(self):
+        from itertools import product
+        from benchmarker import Benchmarker
+        ##from guppy import hpy
+        self.quiet = True
+        debug = self.debug
+        self.debug = False
+        
+        bm = Benchmarker()
+        for p, k in product(xrange(3, 11), xrange(1, 11)):
+            self.param_chars = p
+            self.num_of_keys = k
+            with bm("encrypt/decrypt(p: %d, k: %d)" % (self.param_chars, self.num_of_keys)):
+                encdata = self.encrypt("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz This is plain data."*10, "This is key.")
+                decdata = self.decrypt(encdata, "This is key.")
+            assert decdata == "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz This is plain data."*10
+
+        #hp = hpy()
+        #print hp.heap()
+
+        self.debug = debug
+        self.quiet = False
+        return
+
 if __name__ == "__main__":
     import sys
-    q = HighPowered(sys.argv[1], debug=True)
-    q.encrypt(sys.argv[2])
+    q = HighPowered(debug=True)
+    #q.benchmark()
+    q.encrypt_with_file(plaindata=sys.argv[1], keydata=sys.argv[2], encfile="encdata.bin")
+    
+    print q.decrypt_with_file(encfile="encdata.bin", keydata=sys.argv[2])
